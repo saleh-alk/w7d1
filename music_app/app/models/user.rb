baeab8c998681ea3daa74a1308bf0bd1f3dbd153
Saleh Alkaheli <skalkaheli@gmail.com>
2022-11-15 11:15:40 -0800

    User Models

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ApplicationRecord
+
+    def generate_unique_session_token
+        token = SecureRandom.base64
+
+        while Users.exists?(session_token: token)
+            token = SecureRandom.base64
+        end
+
+        token
+    end
+
+    def reset_session_token
+        self.session_token = generate_unique_session_token
+
+        self.save!
+
+        self.session_token
+
+    end
+
+    def ensure_session_token
+        self.session_token ||= reset_session_token
+    end
 end
