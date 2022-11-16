54feb987cb301c07f410c6295fe97a96d51f796c
Saleh Alkaheli <skalkaheli@gmail.com>
2022-11-15 17:05:05 -0800

    Working on css

---
+++ music_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+        if @user
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            render :new
+        end
+
+    end
+
+    def destroy
+        logout
+        redirect_to new_session_url
+    end
+
+end
--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,10 +1,31 @@
 class User < ApplicationRecord
 
-    def generate_unique_session_token
-        token = SecureRandom.base64
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
 
-        while Users.exists?(session_token: token)
-            token = SecureRandom.base64
+    attr_reader :password
+
+
+
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+
+    end
+
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64(16)
+
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64(16)
         end
 
         token
@@ -22,4 +43,17 @@ class User < ApplicationRecord
     def ensure_session_token
         self.session_token ||= reset_session_token
     end
+
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        bcypt_object = BCrypt::Password.new(self.password_digest)
+        bcypt_object.is_password?(password)
+    end
+
+
 end
